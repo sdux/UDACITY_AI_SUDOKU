a5955bb9e9574b772da7831ae2fba05231f0ea87
S Dux <sduxbury@gmail.com>
2017-09-08 13:28:14 -0600

    initial

--- solution.py
+++ solution.py
@@ -76,7 +76,7 @@ def solve(grid):
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
-
+    print('Entered Main display')
     try:
         from visualize import visualize_assignments
         visualize_assignments(assignments)
